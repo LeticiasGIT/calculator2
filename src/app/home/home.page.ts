c5ea3f05e6d7eb47ac05277d931b44a897c91231
Leticia Maciel <126934799+LeticiasGIT@users.noreply.github.com>
2024-04-25 17:28:11 -0400

    👍

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,15 +14,18 @@ export class HomePage {
   numeroA: any;
   numeroB: any;
   numero: number=0;
+
+  adicionarDigito(digito: number) {
+    this.numero += digito;
+  }
   
-  importarNumero({ numeroA, numeroB }: { numeroA: number; numeroB: number; }) {
+  adicionarOperador(operador: string) {
+    this.operador = operador;
     this.numero = parseFloat(this.numeroA);
     this.numero = parseFloat(this.numeroB);
+    this.operacao = '';
   }
 
-  importarOperador(){
-    this.operacao = "";
-  }
 
   calcular(){
     if (this.operador && this.operacao && this.numeroA !== undefined && this.numeroB !== undefined){
