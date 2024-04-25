94fb6c521d71ae4b4f13d687aa84d988728dbf0a
Leticia Maciel <126934799+LeticiasGIT@users.noreply.github.com>
2024-04-25 16:30:17 -0400

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+
+  operacao="";
+  operador="";
+  numeroA: any;
+  numeroB: any;
+  numero: number=0;
+  
+  importarNumero({ numeroA, numeroB }: { numeroA: number; numeroB: number; }) {
+    this.numero = parseFloat(this.numeroA);
+    this.numero = parseFloat(this.numeroB);
+  }
+
+  importarOperador(){
+    this.operacao = "";
+  }
+
+  calcular(){
+    if (this.operador && this.operacao && this.numeroA !== undefined && this.numeroB !== undefined){
+      this.numero = parseFloat(this.numeroB);
+      let resultado:number =0;
+      switch (this.operador) {
+        case '/':
+          resultado =this.numeroA/this.numeroB;
+          break;
+        case '*':
+          resultado =this.numeroA * this.numeroB;
+          break;
+        case '-':
+          resultado = this.numeroA - this.numeroB;
+          break;
+        case "+":
+          resultado = this.numeroA + this.numeroB;
+          break;
+      }
+      this.operacao = resultado.toString();
+      this.operador = "";
+      this.numeroA = undefined;
+      this.numeroB = undefined;
+    }
+  }
+
+  limpar(){
+    this.operacao = "";
+    this.operador ="";
+    this.numeroA = undefined;
+    this.numeroB = undefined;
+  }
+
+
+}
